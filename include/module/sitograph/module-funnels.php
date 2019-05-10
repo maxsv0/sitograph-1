9845b62c7e9fe069b21a7f5693a2a49dac3963d5
Vitaliy <hodachinskiy@gmail.com>
2019-05-10 06:08:41 +0300

    upd
    ** funnels
    + integrate users to funnels module

--- include/module/sitograph/module-funnels.php
+++ include/module/sitograph/module-funnels.php
@@ -16,19 +16,41 @@ if (!empty($_REQUEST["send_to_crm"])) {
         $lead_data = $resultCheck['data'];
 
         $result = api_funnels_crm_lead_send($lead_data);
-        if ($result['ok']) {
+        if ($result['ok'] && !empty($result["data"])) {
             msv_message_ok('ok');
+            $rowUpd = array();
             
-            var_dump($result['data']);
-            
-            $rowUpd = $result['data'];
+           
+            if (!empty($result['data']['crm_member_id'])) {
+                $rowUpd = $result['data'];
+            } else {
+                $rowUpd['crm_lead_id'] = $result['data']['crm_lead_id'];
+            }
             $rowUpd['id'] = $lead_data['id'];
             $rowUpd['status'] = 1;
-            $rowUpd['status_info'] = $result['ok'];
-
+            $rowUpd['status_info'] = $result["msg"];
+            
             $resultUpdateLead = db_update_row(TABLE_FUNNEL_LEADS, $rowUpd);
-            //var_dump($resultUpdateLead);
+
+            if (!empty($rowUpd['crm_member_id'])) {
+                $rowUpdUser = array();
+                $rowUpdUser['crm_id'] = $rowUpd['crm_member_id'];
+                $rowUpdUser['id'] = $lead_data['member_user_id'];
+
+                $resultUpdateUser = db_update_row(TABLE_USERS, $rowUpdUser);
+
+            }
+
+            var_dump($rowUpd);
+
+
         } else {
+            $rowUpd = array(
+                "id" => $lead_data['id'],
+                "status_info" => $result["msg"],
+            );
+
+            db_update_row(TABLE_FUNNEL_LEADS, $rowUpd);
             msv_message_error($result["msg"]);
         }
     }
